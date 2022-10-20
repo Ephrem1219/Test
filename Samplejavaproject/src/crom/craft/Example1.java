f90f94fbd9f65ae65660f77b3ea3c5e5c52d2c17
Ephrem1219 <ephrem.gezae12@gmail.com>
2022-10-20 19:16:05 -0400

    cjhmbvlj,n

---
+++ Samplejavaproject/src/crom/craft/Example1.java
@@ -0,0 +1,17 @@
+package crom.craft;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+
+	}
+
+}
